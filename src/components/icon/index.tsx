c26ceab926dd97e5db3f98707c0b377707667856
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2023-01-14 10:23:28 +0200

    chore: update projects cards

---
+++ src/components/icon/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Folder from './folder.svg'
+
+const FolderIcon = () => {
+  return (
+    <Folder className="h-10 w-10 text-primary-color-500 dark:text-primary-color-dark-500" />
+  )
+}
+
+export default FolderIcon
---
+++ src/data/projectsData.tsx
@@ -0,0 +1,24 @@
+const projectsData = [
+  {
+    title: 'Sfumato',
+    description: `Digital Market 📱 for artists 🎨 and art lovers. 🖼️`,
+    imgSrc: '',
+    href: '',
+    github: 'https://github.com/davis-shyaka/sfumato',
+    tech1: 'React Native',
+    tech2: 'NodeJS',
+    tech3: 'MongoDB'
+  },
+  {
+    title: 'Library Management System 📖',
+    description: `A management system to assist librarians 🏫 in tracking records of the library`,
+    imgSrc: '',
+    href: '',
+    github: 'https://github.com/davis-shyaka/library-ms-app',
+    tech1: 'PHP Laravel',
+    tech2: 'Tailwind CSS',
+    tech3: 'MySQL'
+  }
+]
+
+export default projectsData
