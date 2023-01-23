d9a3c19c7d3f5c579c542e927a8a32cf49ea53f4
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2023-01-23 16:18:52 +0200

    fix: remove filepond.min.css files from main.tsx b'se filepond is longer used.

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import 'filepond/dist/filepond.min.css';
-import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
